5943b1d5ad74a7af08cb127823d6a931ef8ac5cb
Shamiigamer <86429298+Shamiigamer@users.noreply.github.com>
2021-06-24 10:23:45 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,173 @@
+let Discord = require("discord.js")
+let client = new Discord.Client()
+
+client.on("message", message => {
+if(message.content === "$hi"){
+   message.channel.send("Hello!")  
+} {
+if(message.content === "$how are you") {  
+  message.channel.send("Fine what about you!")
+} 
+   } {
+if(message.content === "$i am fine") {
+  message.channel.send("that's nice")
+}
+   } {
+if(message.content === "$how's you'r day") {
+  message.channel.send("My one is good tell me you'r!")
+}
+   } {
+if(message.content === "$nic3 buddy") {
+  message.channel.send("thanks bro")
+}
+   } {
+if(message.content === "$bye") {
+  message.channel.send("have a good day/night")
+}    
+   } {
+if(message.content === "$wait") {
+  message.channel.send("okok")
+}    
+   } {
+if(message.content === "$can i fuck you") {
+  message.channel.send("why not come to me")
+}    
+   } {
+if(message.content === "$omg") {
+  message.channel.send("yea don't be shame come")
+}     
+   } {
+if(message.content === "$really") {
+  message.channel.send("Yea")
+}    
+   } {
+if(message.content === "$will you marry me") {
+  message.channel.send("yea but first give me pic of you'r dick")
+}    
+   } {
+if(message.content === "$my dick is big") {
+  message.channel.send("okok them i will marry you,where we will marry")
+}     
+   } {
+if(message.content === "$mute") {
+  message.channel.send("mute him who he want to mute mf admin and mod")
+}    
+   } {
+if(message.content === "$why i mute him") {
+  message.channel.send("because my love want to mute him")
+}    
+   } {
+if(message.content === "$XD") {
+  message.channel.send("lol")
+}    
+   } 
+if(message.content === "$wtf")  {
+  message.channel.send("What happen man")  
+  
+  } {
+if(message.content === "$talk with me in urdu")    
+  message.channel.send("mujhy urdu nahi ati")
+  } {
+    if(message.content === "$acha tumhy nahi ati") 
+  message.channel.send("nahi ati na kaha na bc")
+}  {
+if(message.content === "$where") {
+  message.channel.send("In my ass or do you know some good place where me and you aouch")
+} 
+} {
+if(message.content === "$where you will say") {
+  message.channel.send("no where you would say,Then we will marry there")
+} 
+} {
+if(message.content === "$are you a girl") {
+  message.channel.send("Yea i am a girl but also man you can say me shemale")
+} 
+} {
+if(message.content === "$lmao") {
+  message.channel.send("Fuck off")
+} {
+if(message.content === "$OP") {
+  message.channel.send("gg")
+} 
+} {
+if(message.content === "$mute") {
+  message.channel.send("mute")
+} { 
+}  
+} {
+if(message.content === "$fuck") {
+  message.channel.send("You")
+} 
+} {
+if(message.content === "$nothing") {
+  message.channel.send("just chill man")
+} 
+} {
+if(message.content === "$goodnight") {
+  message.channel.send("Sweet Dreams")
+} 
+}  {
+if(message.content === "$app ka nam") {
+  message.channel.send("Bitch")
+}
+} {
+if(message.content === "$nice") {
+  message.channel.send("ty")
+} 
+} {
+if(message.content === "$what do you think") {
+  message.channel.send("i think you both need to marry") 
+}
+} {
+if(message.content === "$hello") {
+  message.channel.send("hi")
+}
+} {
+if(message.content === "$kese ho") {
+  message.channel.send("May thek app sunao")
+} 
+} {
+if(message.content === "$may bi thek") {
+  message.channel.send("acha hy acha hy tu thek to my bi acha hy")
+} 
+} {
+if(message.content === "$or sunao") {
+  message.channel.send("kia sunahn")
+} 
+} {
+if(message.content === "$kuch bi") {
+  message.channel.send("bor krta hy tu bc") 
+} 
+} {
+if(message.content === "$ye kia hy") {
+  message.channel.send("$ye tera....")
+}  
+} {
+if(message.content === "$bc?") {
+  message.channel.send("bc=Bohat cute")
+}
+}  {
+  if(message.content === "$kuch nahi") {
+    message.channel.send("kuch to samjhy 'astogfar'")
+  }
+} {
+  if(message.content === "$saste nashe ko janta hy") {
+    message.channel.send("Hm janta hn sala gohanchu")
+  }
+} {
+  if(message.content === "$abbe saale"){
+    message.channel.send("kia by gohanchu")
+  }
+} 
+}
+if(message.content === "embed") {
+  let embed = new Discord.MessageEmbed()
+  .setTitle("Shamii-Bot-Help ") 
+  .setDescription("Want know how this bot works,before saying something press $ sign")
+  .setColor("RANDOM")
+  .setFooter("DM to Shamii1097")
+  message.channel.send(embed)
+   }  
+}
+ ) 
+client.login("ODQyNjQ4NjcxNDM3NzE3NTQ0.YJ4Xug._9UIYww_HqT-Pt-3FR7WxDcgRTU")
